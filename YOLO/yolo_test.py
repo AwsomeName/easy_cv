5736777f59d27db0fb171add2e1cc0f4a2d2f6ab
AwsomeName <liuchang_hit_cs@gmail.com>
2023-10-10 20:47:55 +0800

    update

---
+++ YOLO/yolo_test.py
@@ -0,0 +1,30 @@
+# ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+from ultralyticsplus import YOLO, render_result
+
+# load model
+# model = YOLO('ultralyticsplus/yolov8s')
+model = YOLO('/home/lc/models/yolo/yolov8s/yolov8s.pt')
+
+# set model parameters
+model.overrides['conf'] = 0.25  # NMS confidence threshold
+model.overrides['iou'] = 0.45  # NMS IoU threshold
+model.overrides['agnostic_nms'] = False  # NMS class-agnostic
+model.overrides['max_det'] = 1000  # maximum number of detections per image
+
+# set image
+# image = 'https://github.com/ultralytics/yolov5/raw/master/data/images/zidane.jpg'
+image = 'zidane.jpg'
+
+# perform inference
+results = model.predict(image)
+print(results)
+
+# observe results
+print("-------")
+print(results[0].boxes)
+# print(results[0].boxes.value)
+render = render_result(model=model, image=image, result=results[0])
+# render.show()
+
+render.save("result.png")
---
+++ readme.md
@@ -0,0 +1,17 @@
+学习了解CV和多模态
+
+1. YOLO模型
+
+2. Segement Anything
+
+3. SLAM
+
+4. Stable_diffusion
+
+5. 换脸方案
+
+
+6. AI生成人物模型
+
+
+7. 多模态模型
